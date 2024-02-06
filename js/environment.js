2461389aa3148ec339fe0c9d6bc9d4ce29c8e5ed
Aebel Shajan <aebelshajan@gmail.com>
2024-02-06 10:42:33 +0000

    added ability to create city

--- js/environment.js
+++ js/environment.js
@@ -2,15 +2,35 @@ import * as THREE from "three";
 
 export default function loadEnvironment(scene) {
     scene.add(createFloor());
-    // adding trees randomly
-    for (let i = 0; i < 100; i++) {
-        const tree = createTree();
-        const min = -500;
-        const max = 500;
-        tree.position.set(getRandomInt(min, max), 0, getRandomInt(min, max));
-        scene.add(tree);
+    createCity(scene);
+}
+
+function createCity(scene) {
+    const city = {
+        citySize: 5,
+        blockSize: 3,
+        buildingLength: 20,
+        roadWidth: 20
     }
 
+    for (let blockX = 0; blockX < city.citySize; blockX++) {
+        for (let blockZ = 0; blockZ < city.citySize; blockZ++) {
+            const blockPlusRoad = (city.blockSize * city.buildingLength) + city.roadWidth;
+            const blockStartPos = new THREE.Vector3(blockPlusRoad * blockX, 0, blockPlusRoad * blockZ);
+            for (let buildingX = 0; buildingX < city.blockSize; buildingX++) {
+                for (let buildingZ = 0; buildingZ < city.blockSize; buildingZ++) {
+                    const buildingOffset = new THREE.Vector3(1, 0, 1).multiplyScalar(city.buildingLength);
+                    const buildingPos = blockStartPos.clone().add(buildingOffset).add(new THREE.Vector3(city.buildingLength * buildingX, 0, city.buildingLength * buildingZ));
+                    let building = createCube();
+                    const buildingHeight = getRandomInt(5, 3 * city.buildingLength);
+                    building.geometry.scale(city.buildingLength, buildingHeight, city.buildingLength)
+                    scene.add(building);
+                    building.position.copy(buildingPos);
+
+                }
+            }
+        }
+    }
 }
 
 function createTree() {
@@ -27,7 +47,12 @@ function createFloor() {
     floor.rotation.x = -90 * Math.PI / 180;
     floor.scale.set(50, 50, 50);
     floor.position.set(0, 0, 0);
-    return floor
+    return floor;
+}
+
+function createCube() {
+    const cube = new THREE.Mesh(new THREE.BoxGeometry, new THREE.MeshStandardMaterial({ color: 0xffffff }));
+    return cube;
 }
 
 function getRandomInt(min, max) {
