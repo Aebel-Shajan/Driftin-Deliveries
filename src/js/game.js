a8f42b2f8b6aa8b41e0e5eb66f4c6b3301a77443
Aebel Shajan <aebelshajan@gmail.com>
2024-02-14 18:10:28 +0000

    refactor camera logic into updateCamera()

--- src/js/game.js
+++ src/js/game.js
@@ -14,13 +14,29 @@ const world = new CANNON.World({
 const cannonDebugger = new CannonDebugger(scene, world, {})
 
 
-// Set up the camera
+// Camera
 const camera = new THREE.PerspectiveCamera(
   75, // field of view
   window.innerWidth / window.innerHeight, // aspect ratio
   0.1, // near clipping plane
   1000 // far clipping plane
 );
+let cameraForward = new THREE.Vector3(0, 1, 1); 
+function getXZ(vector3) {
+  return new THREE.Vector3(vector3.x, 0, vector3.z);
+}
+function updateCamera(player) {
+  let newForward = getXZ(player.getForward()).normalize();
+  const velocity = getXZ(player.getVelocity());
+  const position = player.getPosition();
+  if (Math.abs(velocity.length()) > 0.1) {
+    newForward = velocity.normalize();
+  }
+  newForward.multiplyScalar(-10).add(new THREE.Vector3(0, 2, 0));
+  cameraForward.lerp(newForward, 0.1);
+  camera.position.copy(position.clone().add(cameraForward));
+  camera.lookAt(position);
+}
 
 // Set up the renderer
 const renderer = new THREE.WebGLRenderer({antialias: true});
@@ -37,7 +53,6 @@ await player.init();
 scene.add(player.forceDebug);
 scene.add(player.mesh);
 world.addBody(player.body);
-let cameraForward = player.getForward().multiplyScalar(-10).add(new THREE.Vector3(0, 5, 0));
 const dt = 1/ 60;
 
 // Animation loop
@@ -51,14 +66,7 @@ function animate() {
   player.controlPlayer(c, dt);
 
   // camera 
-  const motionDir = player.getForward().clone();
-  if (player.body.velocity.length() > 0.1) {
-    const velDir = new THREE.Vector3().copy(player.body.velocity).normalize();
-    motionDir.lerp(velDir, 0.5);
-  }
-  cameraForward.lerp(motionDir.multiplyScalar(-10).add(new THREE.Vector3(0, 2, 0)), 0.1);
-  camera.position.copy(player.mesh.position.clone().add(cameraForward));
-  camera.lookAt(player.mesh.position);
+  updateCamera(player);
   effect.render(scene, camera);
 }
 
